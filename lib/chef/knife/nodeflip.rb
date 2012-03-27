05981ccb1eb4d0be637bf081e2a296333eb5101e
Jon Cowie <jcowie@etsy.com>
2012-03-27 16:00:31 +0100

    Fix to search against fqdn not name

--- lib/chef/knife/nodeflip.rb
+++ lib/chef/knife/nodeflip.rb
@@ -25,15 +25,15 @@ module KnifeFlip
         exit 1
       end
 
-      puts "Looking for #{@node_name}"
+      puts "Looking for an fqdn of #{@node_name}"
 
       searcher = Chef::Search::Query.new
-      result = searcher.search(:node, "name:#{@node_name}")
+      result = searcher.search(:node, "fqdn:#{@node_name}")
 
       knife_search = Chef::Knife::Search.new
       node = result.first.first
       if node.nil?
-        puts "Could not find a node named #{@node_name}"
+        puts "Could not find a node with the fqdn of #{@node_name}"
         exit 1
       end
 
@@ -54,7 +54,7 @@ module KnifeFlip
       node.save
 
       knife_search = Chef::Knife::Search.new
-      knife_search.name_args = ['node', "name:#{@node_name}"]
+      knife_search.name_args = ['node', "fqdn:#{@node_name}"]
       knife_search.run
 
     end
--- lib/knife-flip.rb
+++ lib/knife-flip.rb
@@ -1,3 +1,3 @@
 module KnifeFlip
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
